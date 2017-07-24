181285c75720e3fcca9ef5562108828adc6f2942
manit <ermanit007@gmail.com>
2017-07-24 17:27:13 +0530

    design a Xml  forget_ password.Xml  andforget_ New_Password.Xml  with the use of  CardView

---
+++ app/src/main/java/com/grademojo/frontpage/Forget_New_Password.java
@@ -0,0 +1,13 @@
+package com.grademojo.frontpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Forget_New_Password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget__new__password);
+    }
+}
